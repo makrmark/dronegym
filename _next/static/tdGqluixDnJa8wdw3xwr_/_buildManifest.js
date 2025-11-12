759ccc8bfc87c8f65e85cc092579c3cc4338911f
makrmark <makrmark@users.noreply.github.com>
2025-11-12 09:59:42 +0000

    deploy: d29b8abc78ef245b72be8c5acfe55a36d50cb57f

---
+++ _next/static/tdGqluixDnJa8wdw3xwr_/_buildManifest.js
@@ -0,0 +1,27 @@
+self.__BUILD_MANIFEST = {
+  "/": [
+    "static/chunks/a8df10a1c336d1ea.js"
+  ],
+  "/_error": [
+    "static/chunks/f7661533cc730d83.js"
+  ],
+  "/mission/[id]": [
+    "static/chunks/7f729da894400b48.js"
+  ],
+  "__rewrites": {
+    "afterFiles": [],
+    "beforeFiles": [
+      {
+        "source": "/repo//_next/:path+",
+        "destination": "/repo/_next/:path+"
+      }
+    ],
+    "fallback": []
+  },
+  "sortedPages": [
+    "/",
+    "/_app",
+    "/_error",
+    "/mission/[id]"
+  ]
+};self.__BUILD_MANIFEST_CB && self.__BUILD_MANIFEST_CB()
